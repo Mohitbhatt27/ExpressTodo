17f7fac6ddf84b98f84d6b6add5bdfad33739690
Mohit <mohit.peak@gmail.com>
2024-05-12 01:10:45 +0530

    Added nodemon and dotenv

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const { PORT } = require("./config/serverConfig");
 
 const app = express();
-const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,23 +12,18 @@ app.get("/ping", (req, res) => {
 });
 
 app.get("/test", (req, res) => {
-
   res.json({ test: "test" });
   console.log("test");
-})
+});
 
 app.post("/test/:category/:id", (req, res) => {
-
   const { category, id } = req.params;
   console.log(category, id);
   res.send("test success");
-})
-
-
-
+});
 
 app.post("/test", (req, res) => {
-  const  ok  = req.body;
+  const ok = req.body;
   console.log(ok);
   res.send("test success");
 });
@@ -38,7 +32,6 @@ app.post("/testAgain", (req, res) => {
   console.log(req.headers);
   res.send("test success");
 });
-
 
 // app.post("/test", (req, res) => {
 //   const {ok} = req.body;
